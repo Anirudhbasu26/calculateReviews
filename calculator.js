10539405ee7f527eddaeb9acac35570bc2a97923
Anirudhbasu26 <145442152+Anirudhbasu26@users.noreply.github.com>
2024-04-08 17:31:36 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -32,16 +32,14 @@ document.getElementById("reviewForm").addEventListener("submit", function (event
     if (isValid) {
         let num5StarReviewsNeeded = Math.ceil((targetAvgRating * currentReviews - currentAvgRating * currentReviews) / (5 - targetAvgRating));
 
-        // Hide the existing form
         document.getElementById("reviewForm").style.display = "none";
 
-        // Create a container for the result and the buttons
         let resultContainer = document.createElement("div");
         resultContainer.classList.add("result-container");
 
         let resultElement = document.createElement("p");
         resultElement.textContent = "Number of 5-Star Reviews Needed: " + num5StarReviewsNeeded;
-        resultElement.style.textAlign = "center"; // Center-align the text
+        resultElement.style.textAlign = "center"; 
         resultContainer.appendChild(resultElement);
 
         let ctaHeader = document.createElement("h3");
@@ -62,25 +60,21 @@ document.getElementById("reviewForm").addEventListener("submit", function (event
             resultContainer.remove();
             document.getElementById("reviewForm").reset();
             document.getElementById("reviewForm").style.display = "block";
-            // Reset calculator widget size
             let calculatorWidget = document.querySelector(".calculator-widget");
             calculatorWidget.style.width = "320px"; // Initial width
             calculatorWidget.style.height = "auto"; // Initial height
         };
         resultContainer.appendChild(calculateAgainButton);
 
-        // Set height and width of result page
         let calculatorWidget = document.querySelector(".calculator-widget");
         calculatorWidget.style.width = "320px"; // Decreased width
         calculatorWidget.style.height = "auto"; // Decreased height
 
-        // Add margin between elements
         resultElement.style.marginBottom = "20px";
         ctaHeader.style.marginBottom = "20px";
         bookDemoButton.style.marginBottom = "10px";
         calculateAgainButton.style.marginTop = "5px";
 
-        // Append the result container to the existing calculator widget
         calculatorWidget.appendChild(resultContainer);
     }
 });
